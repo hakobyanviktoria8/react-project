65affa77cc934561660d8da09001c73c246cff57
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-03-02 13:39:32 +0400

    updated footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,30 +1,12 @@
-import React from 'react'
-import { useInput } from '../../util/hooks/useInput';
-import "./../../util/styles/Footer.css"
+import React from "react";
+import "./../../util/styles/Footer.css";
 
 function Footer({ mode }) {
-  const [email, resetEmail] = useInput("");
-
-  const handleSubmit = (e) => {
-    // eslint-disable-next-line no-unused-expressions
-    e.preventDefault()
-    alert(email.val);
-    resetEmail();  
-  }
-
   return (
     <div className={mode ? "dark footer" : "footer"}>
-      Footer
-      <form onSubmit={handleSubmit}>
-        <input
-          {...email}
-          type="text"
-          placeholder='Subscribe us...'
-        />
-        <button>Subscribe</button>
-      </form>
+      Last updated at 3/2/2023
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
